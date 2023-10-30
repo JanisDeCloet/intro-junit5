3618cf0026d71d68b955cd2c72e0d970c3ae8362
Janis De Cloet <janis.decloet@dxc.com>
2023-10-30 12:10:34 +0100

    test schrijven

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,15 +1,29 @@
 package guru.springframework;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GreetingTest {
+    Greeting greeting;
+
+    @BeforeAll
+    static void beforeAll() {
+
+    }
+
+    @BeforeEach
+    void setUp() {
+        greeting = new Greeting();
+    }
 
     @Test
     void helloWorld() {
-
+        System.out.println(greeting.helloWorld());
     }
 
     @Test
     void testHelloWorld() {
+        System.out.println(greeting.helloWorld("Janis"));
     }
 }
