543dbd6a27b483167b700fccf534ef1e47ded8ab
Janis De Cloet <janis.decloet@dxc.com>
2023-10-30 11:50:44 +0100

    Setup and rename typo

---
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -0,0 +1,15 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+class GreetingTest {
+
+    @Test
+    void helloWorld() {
+
+    }
+
+    @Test
+    void testHelloWorld() {
+    }
+}
